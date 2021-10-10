df382d7f3dc671dd79aafa83a4267801ed5b4d9d
Cam K <none>
2021-10-10 13:42:53 -0400

    Add project files

---
+++ Source/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord_IRC_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
