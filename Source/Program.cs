f7775532d022edb0565f0ba6b469131a2833a763
Cam K <none>
2021-10-10 14:40:48 -0400

    Add Discord boilerplate

--- Source/Program.cs
+++ Source/Program.cs
@@ -6,13 +6,24 @@ using System.Collections.Generic;
 using Meebey.SmartIrc4net;
 using Newtonsoft.Json;
 
+using Discord;
+using Discord.WebSocket;
+
 namespace Discord_IRC_Sharp
 {
     class Program
     {
         static void Main(string[] args) => new Program().Run().GetAwaiter().GetResult();
-        static IrcClient irc = new IrcClient();
+        
         static RelayConfig config;
+        
+        // IRC
+        static IrcClient irc = new IrcClient();
+
+        // Discord
+        static DiscordSocketClient discord;
+        static SocketGuild guild;
+        static bool isDiscordReady;
 
         public async Task Run()
         {
@@ -30,6 +41,40 @@ namespace Discord_IRC_Sharp
                 Log.Write("No configuration file found! A blank config has been written to config.json");
                 return;
             }
+
+            // Connect to Discord
+            Log.Write("Connecting to Discord...");
+            discord = new DiscordSocketClient();
+            discord.Log += (LogMessage msg) => {
+                Log.Write("Discord.NET: " + msg.Message);
+                return Task.CompletedTask;
+            };
+            discord.Ready += () => { 
+                isDiscordReady = true;
+                return Task.CompletedTask;
+            };
+            await discord.LoginAsync(TokenType.Bot, config.discordToken);
+            await discord.StartAsync();
+
+            while(!isDiscordReady);
+
+            // Check for the Discord server and channels
+            guild = discord.GetGuild(config.discordServerId);
+            if(guild == null) {
+                Log.Write("Unable to find the specified Discord server! Check your configuration.");
+                return;
+            }
+            else 
+                Log.Write("Successfully found Discord server: " + guild.Name);
+
+            foreach(var channel in config.channels) {
+                var discordChannel = (SocketTextChannel)discord.GetChannel(channel.Value);
+                if(discordChannel != null) 
+                    Log.Write("Found Discord channel: " + discordChannel.Name);
+                else
+                    Log.Write("Could not find Discord channel: " + channel.Value);
+            }
+
             
             // Connect to IRC server
             Log.Write("Connecting to IRC...");
@@ -46,6 +91,8 @@ namespace Discord_IRC_Sharp
             irc.OnChannelMessage += OnIRCMessage;
             Log.Write("Connected!");
             irc.Listen(true);
+        
+            await Task.Delay(-1);   // This shouldn't be reached (I think) but it's here just in case
         }
 
         private static void OnIRCMessage(object sender, IrcEventArgs e)
